3cbf2c357223373f1aa1b18798b9233cdfd75920
Simon Santarelli <simisantarelli@gmail.com>
2026-01-06 00:29:14 -0300

    feat: add ServicesShowcase in home new section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from './components/modules/footer/Footer';
 import Hero from './components/modules/hero/Hero';
 import Slider from './components/modules/slider/Slider';
+import ServicesShowcase from './components/modules/ServicesShowcase/ServicesShowcase';
 import Preloader from './components/modules/preloader/Preloader';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <main>
         <Hero />
         <Slider />
+        <ServicesShowcase/>
       </main>
 
       <Footer />
